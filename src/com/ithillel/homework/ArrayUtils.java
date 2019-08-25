4804c27907602e5364e4aebf7ecfaba5810a2e4f
Anton Avgustin <avgustin.anton27@gmail.com>
2019-08-25 13:59:43 +0300

    1.0

---
+++ src/com/ithillel/homework/ArrayUtils.java
@@ -0,0 +1,54 @@
+package com.ithillel.homework;
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+
+    public static void printObjArray(Object[] objArr) {
+        // todo: implement this method
+    }
+
+    public static void printIntArray(int[] intArr) {
+        // todo: implement this method
+    }
+
+    public static int[] sortIntArray(int[] intArr) {
+        return new int[]{1, 2, 3, 4, 5};
+        // todo: implement this method
+    }
+
+    public static int[] findMinAndMaxFromIntArray(int[] intArr) {
+        return new int[]{-1, 1};
+        // todo: implement this method
+    }
+
+    public static String toOneStringInCamelCase(String[] strArr) {
+        return null;
+        // todo: implement this method
+    }
+
+    public static int calculateSumElementsInIntArray(int[] intArr) {
+        return 0;
+        // todo: implement this method
+    }
+
+    public static int[] mergeAndSortTwoIntArray(int[] ints1, int[] ints2) {
+        return new int[0];
+        // todo: implement this method
+    }
+
+    public static int calculateAbsAverageElementValueInIntArray(int[] intArr) {
+        return 0;
+        // todo: implement this method
+    }
+
+    public static int[] removeElementFromIntArray(int i, int[] ints) {
+        return new int[0];
+        // todo: implement this method
+    }
+
+    public static int[] generateRandomIntArrayWithSizeMinMax(int size, int min, int max) {
+        return new int[0];
+        // todo: implement this method
+    }
+}
